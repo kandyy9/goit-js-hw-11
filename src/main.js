8464790be4432516b39b599da4f462d775633aeb
kandyy9 <a.kandy.sk@gmail.com>
2024-07-09 23:18:46 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -11,12 +11,18 @@ const gallery = document.querySelector(".gallery");
 const loader = document.querySelector(".loader");
 const smplGallery = new SimpleLightbox('.gallery-item a', { captions: true, captionsData: 'alt', captionDelay: 250 });
 
+const clearSearchValue = (evt) => {
+  evt.target.elements.search.value = "";
+};
+
 form.addEventListener("submit", (evt) => {
     evt.preventDefault();
     loader.style.display = "block";
     const query = evt.target.elements.search.value.trim();
     if (query.length <= 0) {
         loader.style.display = "none";
+        gallery.innerHTML = "";
+        clearSearchValue(evt);
         return iziToast.error({ title: "Error", message: "Search value is empty", position: "topRight" });
     }
 
@@ -24,10 +30,13 @@ form.addEventListener("submit", (evt) => {
         .then(data => {
             loader.style.display = "none";
             if (data.hits.length === 0) {
+                evt.target.elements.search.value = "";
                 throw new Error("No images found");
             }
             renderGallery(data.hits, gallery);
+            clearSearchValue(evt);
             smplGallery.refresh();
+
         })
         .catch(error => {
             loader.style.display = "none";
@@ -37,5 +46,7 @@ form.addEventListener("submit", (evt) => {
                 message: `Sorry, there are no images matching your search query. Please try again! Error: ${error.message}`, 
                 position: "topRight" 
             });
+            clearSearchValue(evt);
+            gallery.innerHTML = "";
         });
 });
